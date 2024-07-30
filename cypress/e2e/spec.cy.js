cffb2492d80f836251bb10cdfc5c3d32e18e928b
Gonzua <edgar.gonzua@gmail.com>
2024-07-30 03:26:39 -0500

    Fix report an flikenes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,12 @@ const { LoginMethods } = require("./pages/login/login.methods")
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://demoblaze.com/')
+    const usuario = 'random01';
+    const constrasena = 'random01';
+    cy.visit('https://demoblaze.com/index.html')
     cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login('username','password')
+    LoginMethods.login(usuario,constrasena)
+    cy.get('a#nameofuser').should('contains.text',usuario)
     cy.wait(5000)
   })
 })
